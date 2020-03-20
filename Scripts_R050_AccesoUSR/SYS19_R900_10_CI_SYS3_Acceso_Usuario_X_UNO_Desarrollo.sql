8cf2e75a20bca29badb053ae41174c59f10e5ba5
Esteban Gutierrez <48463421+Estebangtz@users.noreply.github.com>
2020-03-20 13:35:06 -0600

    20032020
    scripts para validarf permisos

---
+++ Scripts_R050_AccesoUSR/SYS19_R900_10_CI_SYS3_Acceso_Usuario_X_UNO_Desarrollo.sql
@@ -0,0 +1,68 @@
+-- //////////////////////////////////////////////////////////////
+-- // ARCHIVO:			
+-- //////////////////////////////////////////////////////////////
+-- // BASE DE DATOS:	SYS19_BasicBD_20190325
+-- // MODULO:			USUARIO_X_UNO 
+-- // OPERACION:		LIBERACION / TABLAS 
+-- ////////////////////////////////////////////////////////////// 
+
+USE [ADG18_AdministradoraGas_V9999_R0] 
+GO
+
+-- //////////////////////////////////////////////////////////////
+
+   
+   
+-- //////////////////////////////////////////////////////////////
+--	SELECT *	FROM [dbo].[SYS3_ACCESO_USR_X_UNO]
+
+
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[SYS3_ACCESO_USR_X_UNO]') AND type in (N'U'))
+	DELETE	FROM [dbo].[SYS3_ACCESO_USR_X_UNO]
+GO
+
+
+
+
+
+-- //////////////////////////////////////////////////////////////
+-- //////////////////////////////////////////////////////////////
+-- //////////////////////////////////////////////////////////////
+
+
+
+-- ===============================================
+SET NOCOUNT ON
+-- ===============================================
+-- SELECT * FROM [SYS3_ACCESO_USR_X_UNO]
+
+
+EXECUTE [dbo].[PG_CI_SYS3_ACCESO_USR_X_UNO] 0,2006,0, 2006, 099, 13, 1		-- UNIGAS MATRIZ
+EXECUTE [dbo].[PG_CI_SYS3_ACCESO_USR_X_UNO] 0,2006,0, 2006, 099, 21, 1		-- GASOMATICO
+EXECUTE [dbo].[PG_CI_SYS3_ACCESO_USR_X_UNO] 0,2006,0, 2006, 099, 18, 1		-- GAS CHAPULTEPEC
+													   
+EXECUTE [dbo].[PG_CI_SYS3_ACCESO_USR_X_UNO] 0,2006,0, 2006, 169, 13, 1		-- UNIGAS MATRIZ
+EXECUTE [dbo].[PG_CI_SYS3_ACCESO_USR_X_UNO] 0,2006,0, 2006, 169, 21, 1		-- GASOMATICO
+EXECUTE [dbo].[PG_CI_SYS3_ACCESO_USR_X_UNO] 0,2006,0, 2006, 169, 18, 1		-- GAS CHAPULTEPEC
+EXECUTE [dbo].[PG_CI_SYS3_ACCESO_USR_X_UNO] 0,2006,0, 2006, 169, 14, 1		-- UNIGAS MATRIZ
+EXECUTE [dbo].[PG_CI_SYS3_ACCESO_USR_X_UNO] 0,2006,0, 2006, 169, 15, 1		-- GASOMATICO
+EXECUTE [dbo].[PG_CI_SYS3_ACCESO_USR_X_UNO] 0,2006,0, 2006, 169, 16, 1		-- GAS CHAPULTEPEC
+													   
+EXECUTE [dbo].[PG_CI_SYS3_ACCESO_USR_X_UNO] 0,2006,0, 2006, 169, 34, 1		-- UNIGAS MATRIZ
+EXECUTE [dbo].[PG_CI_SYS3_ACCESO_USR_X_UNO] 0,2006,0, 2006, 169, 35, 1		-- GASOMATICO
+EXECUTE [dbo].[PG_CI_SYS3_ACCESO_USR_X_UNO] 0,2006,0, 2006, 169, 36, 1		-- GAS CHAPULTEPEC
+EXECUTE [dbo].[PG_CI_SYS3_ACCESO_USR_X_UNO] 0,2006,0, 2006, 169, 37, 1		-- UNIGAS MATRIZ
+EXECUTE [dbo].[PG_CI_SYS3_ACCESO_USR_X_UNO] 0,2006,0, 2006, 169, 38, 1		-- GASOMATICO
+EXECUTE [dbo].[PG_CI_SYS3_ACCESO_USR_X_UNO] 0,2006,0, 2006, 169, 39, 1		-- GAS CHAPULTEPEC
+GO
+
+
+-- ===============================================
+SET NOCOUNT OFF
+-- ===============================================
+
+
+
+-- //////////////////////////////////////////////////////////////
+-- //////////////////////////////////////////////////////////////
+-- //////////////////////////////////////////////////////////////
